e9f671ba198cfa597ac200ec5482edb9d3885709
Ted Xiong <tedxng2001@gmail.com>
2022-09-08 16:57:49 -0500

    Initial Commit

---
+++ BarteringApp/BarteringAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarteringAppApp.swift
+//  BarteringApp
+//
+//  Created by Ted Xiong on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BarteringAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarteringApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarteringApp
+//
+//  Created by Ted Xiong on 9/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
